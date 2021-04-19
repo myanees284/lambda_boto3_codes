4cfb487966ad501f3bb539a611ff4933bd32092a
myanees284 <44027805+myanees284@users.noreply.github.com>
2021-04-19 13:32:30 +0400

    Create ec2_without_expected_tag_stop_instance.py

---
+++ ec2_without_expected_tag_stop_instance.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+
+ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    #print(event)
+    ec2_instance_id=event['detail']['instance-id']
+    
+    # Put Logic
+    tag_response= ec2.describe_tags(
+    Filters=[
+        {
+            'Name': 'resource-id',
+            'Values':[ec2_instance_id],
+        },
+    ],
+    )
+
+    alltags=tag_response['Tags']
+    
+    flag='STOP'
+    for item in alltags:
+        print(item['Key'])
+        if item['Key']=='SPECIAL_EXCEPTION':
+            flag='DONT_STOP'
+            break
+            
+    print(flag)    
+    
+    # Decision Making
+    
+    if flag=='STOP':
+        ec2.stop_instances(InstanceIds=[ec2_instance_id])
+        snsarn="arn:aws:sns:us-west-2:557459294105:SuperSNS"
+        errormsg="EC2 "+ ec2_instance_id + " stopped"
+        snsresponse=sns.publish(TopicArn=snsarn,
+                                Message=errormsg,
+                                Subject="EC2 Violated Company Policy")
