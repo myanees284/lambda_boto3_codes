70114033446e6777d66141f9b9e6506ed751cfc5
myanees284 <44027805+myanees284@users.noreply.github.com>
2021-04-25 12:13:24 +0400

    Update Create_Automated_EBS_Backup.py

--- Create_Automated_EBS_Backup.py
+++ Create_Automated_EBS_Backup.py
@@ -7,35 +7,35 @@ def lambda_handler(event, context):
     
     # Get list of regions
     regions = ec2.describe_regions().get('Regions',[] )
-
+   # print(regions)
     # Iterate over regions
     for region in regions:
-        print "Checking region %s " % region['RegionName']
+        print("Checking Region {}".format(str(region['RegionName'])))
         reg=region['RegionName']
-
+        
         # Connect to region
         ec2 = boto3.client('ec2', region_name=reg)
-    
+        
         # Get all in-use volumes in all regions  
         result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}])
         
         for volume in result['Volumes']:
-            print "Backing up %s in %s" % (volume['VolumeId'], volume['AvailabilityZone'])
-        
+            print("Backing up {} in {}".format(volume['VolumeId'], volume['AvailabilityZone']))
+            
             # Create snapshot
             result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
-        
+            
             # Get snapshot resource 
             ec2resource = boto3.resource('ec2', region_name=reg)
             snapshot = ec2resource.Snapshot(result['SnapshotId'])
         
             volumename = 'N/A'
-        
+            
             # Find name tag for volume if it exists
             if 'Tags' in volume:
                 for tags in volume['Tags']:
                     if tags["Key"] == 'Name':
                         volumename = tags["Value"]
-        
+                        
             # Add volume name to snapshot for easier identification
             snapshot.create_tags(Tags=[{'Key': 'Name','Value': volumename}])
